360bfc04b149bdb553ef347d06ff46cb10d79d37
Rob Glaubius <rglaubius@avenirhealth.org>
2024-01-02 10:30:45 -0500

    Expanded the unit test for all-cause deaths to use projected deaths in Malawi per official UNAIDS 2023 HIV estimates.

--- tests/test_all_cause_deaths.py
+++ tests/test_all_cause_deaths.py
@@ -3,13 +3,22 @@ import unittest
 import percussion.all_cause_deaths as all_cause_deaths
 
 class Test_TestAllCauseDeaths(unittest.TestCase):
+    def test_deaths_template(self):
+        deaths = all_cause_deaths.all_cause_deaths(1970)
+        deaths.read_csv("tests/deaths-data-synthetic.csv")
+        template_test = deaths.projection_template()
+        template_base = pd.read_csv("tests/deaths-projected.csv")
+        keycols = ['Year', 'Gender', 'AgeMin', 'AgeMax']
+        self.assertTrue(template_test[keycols].equals(template_base[keycols]))
+
     def test_deaths_lnlhood(self):
         deaths = all_cause_deaths.all_cause_deaths(1970)
         deaths.read_csv("tests/deaths-data-synthetic.csv")
-        template = deaths.projection_template()
-        template['Deaths'] = 3200
-        lnlhood = deaths.likelihood(template)
-        self.assertEqual(lnlhood, -93.10678896101508)
+        proj_mort = pd.read_csv("tests/deaths-projected.csv")
+        lnlhood = deaths.likelihood(proj_mort)
+        ## TODO: Reimplement the likelihood in R using the example data and verify that
+        ## the log-likelihood matches this value
+        self.assertEqual(lnlhood, 8.609445243532194)
 
 if __name__ == "__main__":
     unittest.main()
